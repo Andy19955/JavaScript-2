8e9147244019cef1a481c0e5f9d242818c97417e
Andy19955 <andy_19955@hotmail.com>
2024-12-27 00:59:24 +0100

    Include sign in and sign up pages in router function

--- js/app.js
+++ js/app.js
@@ -5,11 +5,21 @@ function router() {
   const { pathname } = location;
 
   switch (pathname) {
+    case "/":
+    case "/index.html":
+      console.log("sign in");
+      break;
+    case "/register/":
+      console.log("register");
+      break;
     case "/profile/":
+      toggleMenu();
       toggleFollowersFollowing();
+      break;
+    case "/feed/":
+      toggleMenu();
       break;
   }
 }
 
 router();
-toggleMenu();
