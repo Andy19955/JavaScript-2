dafd4b29e8d7954279d06101848bf33446e2483a
Andy19955 <andy_19955@hotmail.com>
2024-12-27 22:21:23 +0100

    Add shared function for displaying result messages

---
+++ js/ui/shared/displayMessage.js
@@ -0,0 +1,29 @@
+export function displayMessage(container, messageType, message) {
+  let parent = container;
+
+  if (typeof container === "string") {
+    parent = document.querySelector(container);
+  }
+
+  parent.replaceChildren();
+
+  let messageClasses = ["font-medium", "p-3", "border-solid", "border-2"];
+  switch (messageType) {
+    case "error":
+      messageClasses.push("bg-red-100", "text-red-900", "border-red-900");
+      break;
+    case "success":
+      messageClasses.push("bg-green-100", "text-green-900", "border-green-900");
+      break;
+  }
+
+  const messageDiv = document.createElement("div");
+  messageDiv.classList.add(...messageClasses);
+
+  const messageContent = document.createElement("p");
+  messageContent.innerText = message;
+
+  messageDiv.append(messageContent);
+  parent.append(messageDiv);
+  parent.classList.remove("hidden");
+}
