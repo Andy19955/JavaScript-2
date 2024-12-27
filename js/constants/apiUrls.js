acc97271f639654318e8642721e76b004eb302a8
Andy19955 <andy_19955@hotmail.com>
2024-12-28 00:37:50 +0100

    Add api urls for fetching posts

--- js/constants/apiUrls.js
+++ js/constants/apiUrls.js
@@ -1,3 +1,4 @@
-const baseUrl = "https://v2.api.noroff.dev/";
-export const registerUrl = `${baseUrl}auth/register/`;
-export const signInUrl = `${baseUrl}auth/login/`;
+const baseUrl = "https://v2.api.noroff.dev";
+export const registerUrl = `${baseUrl}/auth/register`;
+export const signInUrl = `${baseUrl}/auth/login`;
+export const postsUrl = `${baseUrl}/social/posts?_author=true`;
