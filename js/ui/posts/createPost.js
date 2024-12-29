842e8a1f698157eb32c8a7125ce405e9f0ed1d1f
Andy19955 <andy_19955@hotmail.com>
2024-12-30 00:24:58 +0100

    Remove console.log

--- js/ui/posts/createPost.js
+++ js/ui/posts/createPost.js
@@ -1,5 +1,4 @@
 export function createPost(post) {
-  console.log(post);
   const postCard = document.createElement("div");
   postCard.classList.add("overflow-hidden", "relative", "group", "hover:cursor-pointer", "rounded-lg", "h-64");
 
