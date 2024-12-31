8d75a3137b97b935ad31f73f37e1b38448901e43
Andy19955 <andy_19955@hotmail.com>
2024-12-31 02:19:48 +0100

    Add api urls

--- js/constants/apiUrls.js
+++ js/constants/apiUrls.js
@@ -2,3 +2,4 @@ const baseUrl = "https://v2.api.noroff.dev";
 export const registerUrl = `${baseUrl}/auth/register`;
 export const signInUrl = `${baseUrl}/auth/login`;
 export const postsUrl = `${baseUrl}/social/posts`;
+export const profileUrl = `${baseUrl}/social/profiles`;
