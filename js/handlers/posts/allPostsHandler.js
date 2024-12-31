f346b9eb43e8ef08e6d45f4ec9c1242aba2309b4
Andy19955 <andy_19955@hotmail.com>
2024-12-31 02:23:52 +0100

    Remove accessToken and apiKey variables

--- js/handlers/posts/allPostsHandler.js
+++ js/handlers/posts/allPostsHandler.js
@@ -1,20 +1,18 @@
 import { displayMessage } from "../../ui/shared/displayMessage.js";
 import { fetchPosts } from "../../api/posts/fetchPosts.js";
 import { displayPosts } from "../../ui/posts/displayPosts.js";
-import { handleInfiniteScroll } from "../../helpers/infiniteScroll.js";
+import { infiniteScroll } from "../../helpers/infiniteScroll.js";
 
-export async function allPostsHandler(accessToken, apiKey, page) {
+export async function allPostsHandler(page) {
   const postsContainer = document.querySelector("#postsContainer");
   const postsLoader = document.querySelector("#postsLoader");
   try {
-    const posts = await fetchPosts(accessToken, apiKey, page);
+    const posts = await fetchPosts(page);
     postsLoader.classList.add("hidden");
     displayPosts(posts, postsContainer);
   } catch (error) {
     displayMessage("#messageContainer", "error", error.message);
   } finally {
-    window.addEventListener("scroll", function () {
-      handleInfiniteScroll(accessToken, apiKey);
-    });
+    window.addEventListener("scroll", infiniteScroll);
   }
 }
