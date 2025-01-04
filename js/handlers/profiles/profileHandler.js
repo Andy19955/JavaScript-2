40ecfc09732c8f12c07304e7429513c67e79e77d
Andy19955 <andy_19955@hotmail.com>
2025-01-04 20:34:29 +0100

    Remove commented code

--- js/handlers/profiles/profileHandler.js
+++ js/handlers/profiles/profileHandler.js
@@ -1,28 +1,20 @@
 import { displayMessage } from "../../ui/shared/displayMessage.js";
 import { fetchProfile } from "../../api/profiles/fetchProfile.js";
-import { displayPosts } from "../../ui/posts/displayPosts.js";
-import { infiniteScroll } from "../../helpers/infiniteScroll.js";
 import { displayProfile } from "../../ui/profiles/displayProfile.js";
 import { getQueryParam } from "../../helpers/getQueryParam.js";
 import { getName } from "../../helpers/storage.js";
 
 export async function profileHandler() {
-  // const postsContainer = document.querySelector("#postsContainer");
   const profileLoader = document.querySelector("#profileLoader");
   let name = getName();
   const queryName = getQueryParam("name");
   if (queryName) name = queryName;
   try {
     const profile = await fetchProfile(name);
-    profileLoader.classList.add("hidden");
     displayProfile(profile.data);
-    // displayPosts(posts, postsContainer);
   } catch (error) {
-    profileLoader.classList.add("hidden");
     displayMessage("#messageContainer", "error", error.message);
   } finally {
-    // window.addEventListener("scroll", function () {
-    //   handleInfiniteScroll(accessToken, apiKey);
-    // });
+    profileLoader.classList.add("hidden");
   }
 }
