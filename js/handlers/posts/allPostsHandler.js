202d11962e74027948b26585e257dad3ca4db423
Andy19955 <andy_19955@hotmail.com>
2025-01-04 03:16:04 +0100

    Remove console.log()

--- js/handlers/posts/allPostsHandler.js
+++ js/handlers/posts/allPostsHandler.js
@@ -10,7 +10,6 @@ export async function allPostsHandler(page) {
     const posts = await fetchPosts(page);
     postsLoader.classList.add("hidden");
     displayPosts(posts, postsContainer);
-    console.log(posts);
   } catch (error) {
     displayMessage("#messageContainer", "error", error.message);
   } finally {
